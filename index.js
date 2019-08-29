accdaa19fafcf02b2447e09e81faf7f8b77aee91
Alix Axel <alix.axel@gmail.com>
2019-08-29 13:25:12 +0100

    Closes #5

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const escapeStringRegexp = require('escape-string-regexp')
+
 function createWordBoundaryRegex (collection, flags = 'ui') {
   if (!collection) throw new TypeError('Need to provide a collection.')
-  const patterns = collection.map(pattern => `\\b${pattern}\\b`)
-  return RegExp(patterns.join('|'), flags)
+  const patterns = collection.map(pattern => escapeStringRegexp(pattern))
+  return new RegExp(`\\b(?:${patterns.join('|')})\\b`, flags)
 }
 
 module.exports = createWordBoundaryRegex
